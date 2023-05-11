2cc8c3a3cddf64c9c13601aac1404bdba12b0839
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-11 17:11:06 -0600

    Mostrar los paises en pantalla

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -26,9 +26,16 @@ export class CountriesService {
 
     const url: string = `${ this.baseUrl }/region/${ region }?fields=cca3,name,borders`;
 
-    return this.http.get<SmallCountry[]>(url)
+    return this.http.get<Country[]>(url)
       .pipe(
+        map( countries => countries.map( country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        }))),
         tap( response => console.log({response}))
       )
   }
+
+
 }
