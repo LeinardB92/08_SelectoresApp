d2568dca8758e0b3ea36252a311eebd80cbd77d5
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-11 12:07:22 -0600

    Selector de regiones - Primer selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -15,8 +17,10 @@ export class SelectorPageComponent {
     border : ['', Validators.required ],
   });
 
-
   constructor(
-    private fb: FormBuilder
-  ) {}
+    private fb: FormBuilder, private countriesService: CountriesService) {}
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 }
